a0c67f93251b6968724cbe805133e5aa9e42b2c4
valed-dm <dmvaled@gmail.com>
2024-10-02 23:50:58 +0300

    custom JWT pair serializer added
    to users api serializers.py

--- wm_test/users/api/serializers.py
+++ wm_test/users/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from wm_test.users.models import User
 
@@ -11,3 +12,12 @@ class UserSerializer(serializers.ModelSerializer[User]):
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "pk"},
         }
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token["email"] = user.email
+        return token
