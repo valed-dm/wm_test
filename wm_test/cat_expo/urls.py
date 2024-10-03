37e6cf13420c18995afbb17cf69d37d2084de4f8
valed-dm <dmvaled@gmail.com>
2024-10-03 22:25:09 +0300

    cat_expo urls.py created

---
+++ wm_test/cat_expo/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from .views import AnimalViewSet
+from .views import BreedListView
+from .views import ByBreedListView
+
+router = DefaultRouter()
+router.register(r"animals", AnimalViewSet, basename="animal")
+
+app_name = "cat_expo"
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("breeds/", BreedListView.as_view(), name="breeds-list"),
+    path("breed/<int:breed_id>/", ByBreedListView.as_view(), name="animals-by-breed"),
+]
