095c95e82ccbdb5224c790fed2dba7fac34f128e
valed-dm <dmvaled@gmail.com>
2024-10-04 01:33:13 +0300

    cat_expo got RatingViewSet

--- wm_test/cat_expo/views.py
+++ wm_test/cat_expo/views.py
@@ -1,12 +1,19 @@
+from django.db.models import Avg
+from django.db.models import Count
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework import serializers
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Animal
 from .models import Breed
+from .models import Rating
 from .serializers import AnimalSerializer
 from .serializers import BreedSerializer
+from .serializers import RatingSerializer
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -52,3 +59,47 @@ class AnimalViewSet(viewsets.ModelViewSet):
 
     def perform_destroy(self, instance):
         instance.delete()
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        """
+        Override perform_create to prevent users from rating their own animals.
+        """
+        animal = serializer.validated_data["animal"]
+        if animal.user == self.request.user:
+            msg = "You cannot rate your own animal."
+            raise serializers.ValidationError(msg)
+
+        serializer.save(user=self.request.user)
+
+    # Custom action to get average rating and vote count
+    @action(detail=True, methods=["get"], url_path="rating-info")
+    def rating_info(self, request, pk=None):
+        """
+        Returns the average rating and the total number of votes for the given animal.
+        Can be accessed via the URL /api/ratings/<animal_id>/rating-info/.
+        """
+        rating_instance = self.get_object()
+        animal = rating_instance.animal
+
+        ratings = Rating.objects.filter(animal=animal)
+
+        # Get the average rating and total votes
+        avg_rating = ratings.aggregate(Avg("rating"))["rating__avg"]
+        vote_count = ratings.aggregate(Count("rating"))["rating__count"]
+
+        return Response(
+            {
+                "animal": animal.id,
+                "animal_name": animal.nickname.nickname,
+                "animal_breed": animal.breed.breed,
+                "animal_description": animal.description.description,
+                "average_rating": avg_rating or 0,  # Default to 0 if no ratings
+                "vote_count": vote_count or 0,  # Default to 0 if no votes
+            }
+        )
