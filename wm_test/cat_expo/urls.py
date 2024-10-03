e9c835402830d001d362fd78ce35cd9c8fdb2e0e
valed-dm <dmvaled@gmail.com>
2024-10-04 01:30:26 +0300

    cat_expo urls.py got 'ratings'

--- wm_test/cat_expo/urls.py
+++ wm_test/cat_expo/urls.py
@@ -5,9 +5,11 @@ from rest_framework.routers import DefaultRouter
 from .views import AnimalViewSet
 from .views import BreedListView
 from .views import ByBreedListView
+from .views import RatingViewSet
 
 router = DefaultRouter()
 router.register(r"animals", AnimalViewSet, basename="animal")
+router.register(r"ratings", RatingViewSet, basename="rating")
 
 app_name = "cat_expo"
 
