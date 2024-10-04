2c44961777bdd8034b4455d7051d58af75e6674f
valed-dm <dmvaled@gmail.com>
2024-10-04 16:30:49 +0300

    'KittenViewSet' replaced 'AnimalViewSet'

--- wm_test/cat_expo/urls.py
+++ wm_test/cat_expo/urls.py
@@ -2,13 +2,13 @@ from django.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import AnimalViewSet
 from .views import BreedListView
 from .views import ByBreedListView
+from .views import KittenViewSet
 from .views import RatingViewSet
 
 router = DefaultRouter()
-router.register(r"animals", AnimalViewSet, basename="animal")
+router.register(r"kittens", KittenViewSet, basename="kitten")
 router.register(r"ratings", RatingViewSet, basename="rating")
 
 app_name = "cat_expo"
@@ -16,5 +16,5 @@ app_name = "cat_expo"
 urlpatterns = [
     path("", include(router.urls)),
     path("breeds/", BreedListView.as_view(), name="breeds-list"),
-    path("breed/<int:breed_id>/", ByBreedListView.as_view(), name="animals-by-breed"),
+    path("breed/<int:breed_id>/", ByBreedListView.as_view(), name="kittens-by-breed"),
 ]
